87437171527cc5c5acd2fcd4f188474db56180b3
NawazAsghar <81699613+NawazAsghar@users.noreply.github.com>
2026-08-27 07:34:02 +0500

    add / and fixed the favicon path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-
   return {
+    base: './',
     plugins: [
       react(),
       tailwindcss(),
